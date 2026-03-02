7eb6a9e29baac0c88ff239691c5869c62ffd04af
kalimbachstudy <260343397+kalimbachstudy@users.noreply.github.com>
2026-03-02 12:05:57 +0700

    прилипание

--- java.js
+++ java.js
@@ -2,25 +2,41 @@
 
     let coorX;
     let coorY;
-
-    const drag_el = document.querySelector(".container");
-    const drzone = document.querySelector(".dropzone");
-    drag_el.draggable=true;
-    
-    drag_el.addEventListener('dragstart', (e) =>{
-        e.dataTransfer.setData('text/html', 'dragstart');
-        coorX=e.offsetX;
-        coorY=e.offsetY;
+    const distance = 60;
+    let dragggable = false;
+    const active_el = document.querySelector(".active");
+    const still = document.querySelector(".still");
+    active_el.draggable = true;
+    active_el.addEventListener('mousedown',e=>{
+        dragggable = true;
+        coorX = e.clientX - active_el.offsetLeft;
+        coorY = e.clientY - active_el.offsetTop;
     });
-    drzone.addEventListener('dragover', (e)=>{
+    document.addEventListener('mousemove',e=>{
+        if(!dragggable) return;
+        let x = e.clientX - coorX;
+        let y = e.clientY - coorY;
+
+        const target = still.getBoundingClientRect();
+        const area = document.getElementById('area').getBoundingClientRect();
+
+        const still_x = still.offsetLeft + still.offsetWidth;
+        const still_y = still.offsetTop;
+
+        if(Math.abs(x - still_x)<distance && Math.abs(y - still_y)<distance){
+            x = still_x;
+            y = still_y;
+        }
+        active_el.style.left = x +'px';
+        active_el.style.top = y +'px';
+    });
+    document.addEventListener('mouseup', e=>{
+        dragggable = false;
+    });
+    active_el.addEventListener('dragstart', e => {
         e.preventDefault();
     });
-    drzone.addEventListener('drop', (e)=>{
-        drag_el.style.position = 'absolute';
-        drag_el.style.top = (e.pageY - coorY) +'px';
-        drag_el.style.left = (e.pageX - coorX) +'px';
-    })
+
+    
 })();
 
-slot.addEventListener('dragover', allowDrop);
-slot.addEventListener('drop', handleDrop);
